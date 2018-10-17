e576ea5a09101a3dc88ff738f7abdcf2cadd8d27
rongmingye <1164898915@qq.com>
2018-10-17 17:31:45 +0800

    修改时间

--- httpServer.js
+++ httpServer.js
@@ -1,3 +1,5 @@
+// 项目创建时间 2018/04/22
+
 var express = require('express'); // 快速构建服务器
 var app = express();
 var routesAdmin = require('./serve/routesAdmin.js');
@@ -9,13 +11,11 @@ var urlencodedParser = bodyParser({extended: false});
 var HOST = "192.168.123.163";  // http服务器的ip和端口
 var PORT = "6868";
 
-var developState = "/public"; //开发时是"/public", 打包后用"/build"
-
 app.use(express.static(__dirname + developState)); //views路径
 routesAdmin(app);
 
-app.get('*', function(req, res){
-    res.sendFile(__dirname+'/build/index.html');
+app.get('/', function(req, res){
+    res.sendFile(__dirname+'/views/login.html');
 });
 
 // 监听端口
